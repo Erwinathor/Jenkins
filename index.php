aa0cac3c0d98e7fe7af07da9f0d218da6d0d1118
Erwinwe <46902672+Erwinwe@users.noreply.github.com>
2020-10-26 16:41:27 +0100

    f

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World maal 2 maal 14 maal 50</p>'; ?> 
+ <?php echo '<p>DIT IS AUTOMATISCH GEBEURD DANKZIJ DE GIT POLLER</p>'; ?> 
  </body>
 </html>
