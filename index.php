54db51570175982fd2e0c228306ea4c66cf332bf
Erwinwe <46902672+Erwinwe@users.noreply.github.com>
2020-10-26 16:44:52 +0100

    f

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>DIT IS AUTOMATISCH GEBEURD DANKZIJ DE GIT POLLER</p>'; ?> 
+ <?php echo '<p>DIT IS AUTOMATISCH GEBEURD DANKZIJ DE GIT POLLER werkt dit?</p>'; ?> 
  </body>
 </html>
